61acc60d494a49660ce231ea49d4e2d6b9de0e8e
Ponomarev Stepan <htconesuper11@gmail.com>
2020-05-01 15:22:50 +0300

    Added service

--- src/main/java/spbstu/project/varann/service/VariationService.java
+++ src/main/java/spbstu/project/varann/service/VariationService.java
@@ -1,19 +1,45 @@
 package spbstu.project.varann.service;
 
 import org.springframework.stereotype.Service;
-import spbstu.project.varann.Variation;
-import spbstu.project.varann.VariationID;
+import org.springframework.web.multipart.MultipartFile;
+import spbstu.project.varann.domain.Variation;
+import spbstu.project.varann.domain.VariationID;
 import spbstu.project.varann.VariationRepository;
+import spbstu.project.varann.parser.VcfParser;
 
 @Service
-public class VariantService {
+public class VariationService {
   private final VariationRepository repository;
+  private final VcfParser parser;
 
-  public VariantService(VariationRepository repository) {
+  public VariationService(VariationRepository repository, VcfParser parser) {
     this.repository = repository;
+    this.parser = parser;
   }
 
   public Variation get(VariationID variationID) {
     return repository.findById(variationID).get();
+  }
+
+  public void post(MultipartFile file) {
+    try {
+      var variations = parser.parse(file.getInputStream());
+
+      variations.forEach(variation -> {
+
+        var newVariation = Variation.builder()
+            .chrom(variation.getContig())
+            .pos(variation.getStart())
+            .ref(variation.getReference().getDisplayString())
+            .alt(variation.getAlternateAlleles().toString())
+            .info(variation.getCommonInfo().getAttributes().toString())
+            .build();
+
+        repository.save(newVariation);
+      });
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
